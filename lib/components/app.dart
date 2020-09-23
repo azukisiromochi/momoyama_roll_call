4321eb6583401bd6f9eb00fff97378389c37ff6b
@azukisiromochi <azukisiromochi@gmail.com>
2020-09-23 18:45:37 +0900

    ♲ 大雑把にプロジェクトの構成を変更

---
+++ lib/components/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'home/home.dart';
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const Home(),
+    );
+  }
+}
